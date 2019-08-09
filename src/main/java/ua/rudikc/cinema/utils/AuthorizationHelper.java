fe64fd9cf8dc820d608bb9bd23f4271ab60d0101
Maxim Rudichenko <mrudikc@gmail.com>
2019-08-09 19:46:15 +0300

    Added util class for authorization and deleted test bundles

---
+++ src/main/java/ua/rudikc/cinema/utils/AuthorizationHelper.java
@@ -0,0 +1,22 @@
+package ua.rudikc.cinema.utils;
+
+import java.util.regex.Pattern;
+
+public class AuthorizationHelper {
+
+
+
+    public static boolean isPasswordCorrect(String password){
+
+        //Minimum eight characters, at least one letter and one number pattern:
+        Pattern pattern = Pattern.compile("\"^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$\"");
+
+        return pattern.matcher(password).find();
+    }
+    public static boolean isEmailCorrect(String email){
+
+        //Email validation pattern
+        Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        return pattern.matcher(email).find();
+    }
+}
