7fa9c5fc160c2731b67163df3bddb5a825c992de
Maxim Rudichenko <mrudikc@gmail.com>
2019-08-09 21:49:50 +0300

    Added some tests

---
+++ src/test/java/ua/rudikc/cinema/command/GetTitlesCommandTest.java
@@ -0,0 +1,26 @@
+package ua.rudikc.cinema.command;
+
+import org.junit.Test;
+import ua.rudikc.cinema.dao.exception.DaoException;
+import ua.rudikc.cinema.dao.sqlimplementation.FilmSqlDao;
+import ua.rudikc.cinema.factory.DaoFactory;
+import ua.rudikc.cinema.model.Film;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class GetTitlesCommandTest {
+
+    @Test
+    public void execute() throws DaoException {
+        FilmSqlDao dao = (FilmSqlDao) DaoFactory.getDao("filmDao");
+        ArrayList<Film> films = dao.findActualFilms();
+        if (films.isEmpty()){
+            System.out.println("Hi");
+        }
+        for (Film film : films) {
+            System.out.println(film);
+        }
+    }
+}
---
+++ src/test/java/ua/rudikc/cinema/db/ConnectionPoolTest.java
@@ -0,0 +1,13 @@
+package ua.rudikc.cinema.db;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConnectionPoolTest {
+
+    @Test
+    public void getConnection() {
+
+    }
+}
