e091dd939f09df1e597342c1daeede24e7c153fc
Maxim Rudichenko <mrudikc@gmail.com>
2019-08-13 03:37:55 +0300

    Added constant names of Dao Factory

--- src/main/java/ua/rudikc/cinema/factory/DaoFactory.java
+++ src/main/java/ua/rudikc/cinema/factory/DaoFactory.java
@@ -6,17 +6,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DaoFactory {
+
+
+    public static final String FILM_DAO = "filmDao";
+    public static final String FILM_GENRE_DAO = "filmGenreDao";
+    public static final String ORDER_DAO = "orderDao";
+    public static final String SEAT_DAO = "seatDao";
+    public static final String SEAT_TYPE_DAO = "seatTypeDao";
+    public static final String SEANCE_DAO = "seanceDao";
+    public static final String TICKET_DAO = "ticketDao";
+    public static final String USER_DAO = "userDao";
+
     private static Map<String, Object> daoMap = new HashMap<>();
 
     static {
-        daoMap.put("filmDao", new FilmSqlDao());
-        daoMap.put("filmGenreDao", new FilmGenreSqlDao());
-        daoMap.put("orderDao", new OrderSqlDao());
-        daoMap.put("seatDao", new SeatSqlDao());
-        daoMap.put("seatTypeDao", new SeatTypeSqlDao());
-        daoMap.put("seanceDao", new SeanceSqlDao());
-        daoMap.put("ticketDao", new TicketSqlDao());
-        daoMap.put("userDao", new UserSqlDao());
+        daoMap.put(FILM_DAO, new FilmSqlDao());
+        daoMap.put(FILM_GENRE_DAO, new FilmGenreSqlDao());
+        daoMap.put(ORDER_DAO, new OrderSqlDao());
+        daoMap.put(SEAT_DAO, new SeatSqlDao());
+        daoMap.put(SEAT_TYPE_DAO, new SeatTypeSqlDao());
+        daoMap.put(SEANCE_DAO, new SeanceSqlDao());
+        daoMap.put(TICKET_DAO, new TicketSqlDao());
+        daoMap.put(USER_DAO, new UserSqlDao());
     }
 
     public static Object getDao(String name){
