d05070aaa43398ce69dbd66fa8e5065994350bf5
Maxim Rudichenko <mrudikc@gmail.com>
2019-08-17 22:53:03 +0300

    Added remove order action

---
+++ src/main/java/ua/rudikc/cinema/controller/actions/RemoveOrderAction.java
@@ -0,0 +1,28 @@
+package ua.rudikc.cinema.controller.actions;
+
+import ua.rudikc.cinema.entity.User;
+import ua.rudikc.cinema.factory.CommandFactory;
+import ua.rudikc.cinema.factory.ServiceFactory;
+import ua.rudikc.cinema.service.OrderService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RemoveOrderAction implements Action{
+
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        OrderService orderService = (OrderService) ServiceFactory.getService("orderService");
+        User user = (User) request.getSession().getAttribute("user");
+        int orderId = Integer.parseInt(request.getParameter("order-id"));
+        orderService.removeOrder(user,orderId);
+
+
+
+
+
+
+        return CommandFactory.defineCommand("/user-profile").execute(request,response);
+    }
+}
